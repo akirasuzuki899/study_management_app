787e74e3375a2ef31b8d91b6e9aace7880e40abd
akirasuzuki899 <akira9238pg@gmail.com>
2021-02-26 00:08:25 +0900

    ユーザー登録、編集時に名前の操作を有効化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller? 
+
+  private
+    #ユーザー登録時に名前を登録できるようにする
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up,keys:[:name])
+    end
 end
