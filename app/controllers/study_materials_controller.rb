8876658959ee38ecb71ce7abd26aafdecaa5d027
akirasuzuki899 <akira9238pg@gmail.com>
2021-02-28 18:15:35 +0900

    教材の編集機能を追加

--- app/controllers/study_materials_controller.rb
+++ app/controllers/study_materials_controller.rb
@@ -1,7 +1,6 @@
 class StudyMaterialsController < ApplicationController
   def index
-    @study_material = current_user.study_materials.first
-    # byebug
+    @study_materials = current_user.study_materials
   end
 
   def new
@@ -14,9 +13,25 @@ class StudyMaterialsController < ApplicationController
     redirect_to study_materials_path
   end
 
+  def edit
+    @study_material = StudyMaterial.find(params[:id])
+  end
+
+  def update
+    @study_material = StudyMaterial.find(params[:id])
+    @study_material.update(study_material_params)
+    redirect_to study_materials_path
+  end
+
+  def destroy
+    StudyMaterial.find(params[:id]).destroy
+    redirect_to study_materials_path
+  end
+
+
+
   private
     def study_material_params
       params.require(:study_material).permit(:name, :picture)
     end
-  
 end
