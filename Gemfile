b3afb99aa304024006e7428af3f9588c4a493389
akirasuzuki899 <akira9238pg@gmail.com>
2021-03-04 23:32:17 +0900

    study_materialにバリデーションを追加

--- Gemfile
+++ Gemfile
@@ -21,6 +21,8 @@ gem 'jbuilder', '~> 2.7'
 gem 'devise'
 
 gem 'kaminari'
+
+gem 'activestorage-validator'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
--- app/models/study_material.rb
+++ app/models/study_material.rb
@@ -2,5 +2,10 @@ class StudyMaterial < ApplicationRecord
   belongs_to :user
   has_many :study_notes, dependent: :destroy
   has_one_attached :picture
+  validates :user_id, presence: true
+  validates :name, presence: true
+  validates :picture,
+    presence: true, blob: { content_type: :image, size_range: 1..5.megabytes }
+
 
 end
