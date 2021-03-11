e2c713de208cd14a0d0352a5469372eae78b1b48
akirasuzuki899 <akira9238pg@gmail.com>
2021-03-12 00:11:45 +0900

    user_spec を修正、FactoryBotにUserを作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
-  factory :user do
-    name { 'MyString' }
+  factory :new_user, class: User do
+    name { 'newUser' }
+    email { 'new_email@test.com' }
+    password { 'password' }
+    password_confirmation { 'password' }
+  end
+
+  factory :user, class: User do
+    name { 'existUser' }
+    email { 'exist_email@test.com' }
+    password { 'password' }
+    password_confirmation { 'password' }
   end
 end
