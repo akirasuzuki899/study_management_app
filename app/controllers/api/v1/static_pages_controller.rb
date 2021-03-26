d0ea576f9a8fe6474dc3d320da34068dd71ad1f0
akirasuzuki899 <akira9238pg@gmail.com>
2021-03-26 18:34:01 +0900

    api化-コントローラーの構成とルーティングを変更

---
+++ app/controllers/api/v1/static_pages_controller.rb
@@ -0,0 +1,7 @@
+module Api
+  module V1
+    class StaticPagesController < ApplicationController
+      def home; end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,30 @@
 Rails.application.routes.draw do
-  resources :study_notes
-  resources :study_materials do
-    collection do
-      get :search
-    end
-    member do
-      patch :is_complete
+  devise_for :users
+  namespace :api do
+    namespace :v1 do
+      resources :study_notes
+      resources :study_materials do
+        collection do
+          get :search
+        end
+        member do
+          patch :is_complete
+        end
+      end
+      root 'static_pages#home'
     end
   end
+  # resources :study_notes
+  # resources :study_materials do
+  #   collection do
+  #     get :search
+  #   end
+  #   member do
+  #     patch :is_complete
+  #   end
+  # end
 
-  devise_for :users
-  root 'static_pages#home'
+  # devise_for :users
+  # root 'static_pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
