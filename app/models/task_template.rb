4654d5873899c46c44704c70d64fa4410805f9c6
akirasuzuki899 <akira9238pg@gmail.com>
2021-05-25 00:45:18 +0900

    tast template バリデーションを修正

--- app/models/task_template.rb
+++ app/models/task_template.rb
@@ -11,27 +11,16 @@ class TaskTemplate < ApplicationRecord
   validates :end_time, presence: true
   validates :is_until_tomorrow, inclusion: {in: [true, false]}
 
-  # validate :start_time_should_be_before_end_time
-  # validate :time_shoud_be_less_than24h
-  # validates :time_shoud_be_more_than15m
-
-  # def start_time_should_be_before_end_time 
-  #   if self.is_until_tomorrow == false && self.start_time > self.end_time then
-  #     errors.add(:end_time, "は開始時刻より遅い時間を選択してください")
-  #   end
-  # end
-
-  # def time_shoud_be_less_than24h
-  #     errors.add(:total_time, "合計時間は24時間以内にしてください") if self.start_time == self.end_time
-  # end
+  validate :time_shoud_be_more_than_15min
 
   def time_shoud_be_more_than_15min
     if until_tomorrow? then 
-      day_sec = 86400
-      errors.add(:total_time, "合計時間は24時間以内にしてください") if day_sec - self.start_time + self.end_time > 15*60
-    end
+      total_time_sec = self.end_time.tomorrow - self.start_time
+      errors.add(:total_time, "合計時間は15分以上にしてください") if total_time_sec < 15*60
+    else
       total_time_sec = self.end_time - self.start_time
-      errors.add(:total_time, "合計時間は24時間以内にしてください") if total_time_sec < 15*60
+      errors.add(:total_time, "合計時間は15分以上にしてください") if total_time_sec < 15*60
+    end
   end
 
   def until_tomorrow?
