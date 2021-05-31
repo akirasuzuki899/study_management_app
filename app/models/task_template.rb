fcc2916782f328c3ab4f4a1b397dc9520173b464
akirasuzuki899 <akira9238pg@gmail.com>
2021-05-31 23:56:28 +0900

    日曜日から月曜日にまたぐタスクの表示を有効化

--- app/models/task_template.rb
+++ app/models/task_template.rb
@@ -1,11 +1,11 @@
 class TaskTemplate < ApplicationRecord
   include TaskBase
   include Rails.application.routes.url_helpers
-  before_validation :set_start_date
-
+  
   belongs_to :user
   belongs_to :study_material
-
+  
+  before_validation :set_start_date
   validates :user_id, presence: true
   validates :study_material_id, presence: true
   validates :name, presence: true, length: { maximum: 50 }
@@ -13,7 +13,7 @@ class TaskTemplate < ApplicationRecord
   validates :start_time, presence: true
   validates :end_time, presence: true
   validate :time_should_be_more_than_15min
-  validate :sunday_task_should_be_end_by_midnight
+  # validate :sunday_task_should_be_end_by_midnight
 
   BASEWEEK = { 
     "月" => "2000-01-03" , 
@@ -25,10 +25,10 @@ class TaskTemplate < ApplicationRecord
     "日" => "2000-01-09" , 
   }
 
-  def sunday_task_should_be_end_by_midnight
-    return if self.start_time.nil? || self.end_time.nil?
-    errors.add(:total_time, "日曜日の予定は当日の範囲で選択してください") if self.day_of_week == "日" && until_tomorrow?
-  end
+  # def sunday_task_should_be_end_by_midnight
+  #   return if self.start_time.nil? || self.end_time.nil?
+  #   errors.add(:total_time, "日曜日の予定は当日の範囲で選択してください") if self.day_of_week == "日" && until_tomorrow?
+  # end
 
   def set_start_date
     self.start_date = BASEWEEK[day_of_week]
--- app/serializers/api/v1/task_template_serializer.rb
+++ app/serializers/api/v1/task_template_serializer.rb
@@ -2,11 +2,35 @@ module Api
   module V1
     class TaskTemplateSerializer < ActiveModel::Serializer
       include TaskBase
-      attributes :id, :user_id, :study_material_id, :name, :day_of_week, :start, :end, :start_time, :end_time
+      attributes :id, :user_id, :study_material_id, :name, :day_of_week, :start, :end, :start_time, :end_time, :prev_week_sunday_task
 
       belongs_to :user
       belongs_to :study_material
 
+      def prev_week_sunday_task
+        if object.day_of_week == "日" then
+          {
+            id: object.id,
+            user_id: object.user_id,
+            study_material_id: object.study_material_id,
+            name: object.name,
+            day_of_week: object.day_of_week,
+            start: prev_week_sunday_task_start,
+            end: prev_week_sunday_task_end,
+            start_time: start_time,
+            end_time: end_time,
+          }
+        end        
+      end
+
+      def prev_week_sunday_task_start
+        I18n.l(Time.parse(self.start) - 7.days, format: :dateTime)
+      end
+
+      def prev_week_sunday_task_end
+        I18n.l(Time.parse(self.end) - 7.days, format: :dateTime)
+      end
+      
     end
   end
 end
