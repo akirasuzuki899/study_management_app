94f415b01566ce6222946329dd7c9b050e3288e6
akirasuzuki899 <akira9238pg@gmail.com>
2021-07-23 21:20:06 +0900

    docker 関連ファイルを修正、action text のテストを削除

--- back/spec/rails_helper.rb
+++ back/spec/rails_helper.rb
@@ -31,35 +31,7 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 
-Capybara.register_driver :remote_chrome do |app|
-  url = 'http://chrome:4444/wd/hub'
-  caps = ::Selenium::WebDriver::Remote::Capabilities.chrome(
-    'goog:chromeOptions' => {
-      'args' => [
-        'no-sandbox',
-        # "headless",
-        'disable-gpu',
-        'window-size=1680,1050'
-      ]
-    }
-  )
-  Capybara::Selenium::Driver.new(app, browser: :remote, url: url, desired_capabilities: caps)
-end
-
 RSpec.configure do |config|
-  # ActionTextをテストする
-  config.include ActionTextHelper, type: :system
-
-  config.before(:each, type: :system) do
-    driven_by :rack_test
-  end
-
-  config.before(:each, type: :system, js: true) do
-    driven_by :remote_chrome
-    Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
-    Capybara.server_port = 4000
-    Capybara.app_host = "http://#{Capybara.server_host}:#{Capybara.server_port}"
-  end
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
