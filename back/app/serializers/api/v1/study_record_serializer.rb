1d8ac2c59a9d03da46c92878b8e8460205686485
akirasuzuki899 <akira9238pg@gmail.com>
2021-09-23 17:10:20 +0900

    fix: study_record の日付の扱い方を変更(start_time, end_time に日付情報を追加)

--- back/app/serializers/api/v1/study_record_serializer.rb
+++ back/app/serializers/api/v1/study_record_serializer.rb
@@ -1,9 +1,16 @@
 module Api
   module V1
     class StudyRecordSerializer < ActiveModel::Serializer
-      include TaskBase
       attributes :id, :user_id, :study_material_id, :task_id, :start, :end, :is_finished
 
+      def start
+        object.start_time ? I18n.l(object.start_time, format: :dateTime) : nil
+      end
+      
+      def end
+        object.start_time ? I18n.l(object.end_time, format: :dateTime) : nil
+      end
+      
     end
   end
 end
