9078d6f604662d94bbae388246ace670e87da1d3
akirasuzuki899 <akira9238pg@gmail.com>
2021-10-02 15:56:00 +0900

    feat: weekly, monthly のチャートを作成

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -29,7 +29,9 @@ Rails.application.routes.draw do
       end
       resources :charts do
         collection do
+          get :daily
           get :weekly
+          get :monthly
         end
       end
       get 'health_check', to: 'health_check#index'
