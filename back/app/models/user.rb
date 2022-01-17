a52807a70f93cb842062a708a0fe00839076dd86
akirasuzuki899 <akira9238pg@gmail.com>
2022-01-17 18:59:05 +0900

    不要なコード(コメントアウト)を削除

--- back/app/models/user.rb
+++ back/app/models/user.rb
@@ -1,13 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules.
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable
   include DeviseTokenAuth::Concerns::User
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  #   devise :database_authenticatable, :registerable,
-  #          :recoverable, :rememberable, :validatable
   has_many :study_materials, dependent: :destroy
   has_many :study_notes, dependent: :destroy
   has_many :task_templates, dependent: :destroy
