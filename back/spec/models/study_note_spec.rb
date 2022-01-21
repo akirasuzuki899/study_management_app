4a85e58e37615db6c0253c9e19d738319aa8203e
akirasuzuki899 <akira9238pg@gmail.com>
2022-01-21 23:48:39 +0900

    model(StudyNote) を追加

--- back/spec/models/study_note_spec.rb
+++ back/spec/models/study_note_spec.rb
@@ -1,58 +1,54 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe StudyNote, type: :model do
-#   include ActionDispatch::TestProcess::FixtureFile
-#   before do
-#     @user = FactoryBot.create(:user)
-#     @study_material = FactoryBot.create(:study_material, user_id: @user.id)
-#     @study_material.image = fixture_file_upload('app/assets/images/toeic1.jpg')
-#     @study_note = FactoryBot.build(:study_note, user_id: @user.id, study_material_id: @study_material.id)
-#   end
+RSpec.describe StudyNote, type: :model do
+  include ActionDispatch::TestProcess::FixtureFile
+  let(:user) { FactoryBot.create(:user) }
+  let(:study_material) { FactoryBot.create(:study_material, user_id: user.id) }
+  let(:study_material_note) { FactoryBot.create(:study_material_note, user_id: user.id, noteable_id: study_material.id) }
 
-#   describe '登録' do
-#     context '有効な投稿' do
-#       it '有効な user_id, study_material_id, content, title であれば登録できる' do
-#         expect(@study_note).to be_valid
-#       end
 
-#       it 'title が50文字以下であれば登録できる' do
-#         @study_note.title = 'a' * 50
-#         expect(@study_note).to be_valid
-#       end
-#     end
+  describe '作成' do
+    context '有効な投稿' do
+      it 'user_id, noteable_id, noteable_type, title, rich_text が存在すれば有効' do
+        expect(study_material_note).to  be_valid
+      end
 
-#     context '無効な投稿' do
-#       # content のバリデーションが少ない（文字数、画像のサイズ、フォーマットなど未実装）
+      it 'title が50文字以下であれば登録できる' do
+        study_material_note.title = 'a' *50
+        expect(study_material_note).to  be_valid
+      end
+    end
 
-#       it 'ユーザーが存在しない場合は登録できない' do
-#         @study_note.user_id = nil
-#         @study_note.valid?
-#         expect(@study_note.errors[:user_id]).to include("can't be blank")
-#       end
+    context '無効な投稿' do
+      it 'ユーザーが存在しない場合は登録できない' do
+        study_material_note.user_id = ''
+        study_material_note.valid?
+        expect(study_material_note.errors[:user_id]).to include("can't be blank")
+      end
 
-#       it '教材が存在しない場合は登録できない' do
-#         @study_note.study_material_id = nil
-#         @study_note.valid?
-#         expect(@study_note.errors[:study_material_id]).to include("can't be blank")
-#       end
+      it 'title が空であれば登録できない' do
+        study_material_note.title = ''
+        study_material_note.valid?
+        expect(study_material_note.errors[:title]).to include("can't be blank")
+      end
 
-#       it 'title が空であれば登録できない' do
-#         @study_note.title = nil
-#         @study_note.valid?
-#         expect(@study_note.errors[:title]).to include("can't be blank")
-#       end
+      it 'title が51文字以上であれば登録できない' do
+        study_material_note.title = 'a' * 51
+        study_material_note.valid?
+        expect(study_material_note.errors[:title]).to include('is too long (maximum is 50 characters)')
+      end
 
-#       it 'content が空であれば登録できない' do
-#         @study_note.content = nil
-#         @study_note.valid?
-#         expect(@study_note.errors[:content]).to include("can't be blank")
-#       end
+      it 'noteable_id が空であれば登録できない' do
+        study_material_note.noteable_id = ''
+        study_material_note.valid?
+        expect(study_material_note.errors[:noteable_id]).to include("can't be blank")
+      end
 
-#       it 'title が51文字以上であれば登録できない' do
-#         @study_note.title = 'a' * 51
-#         @study_note.valid?
-#         expect(@study_note.errors[:title]).to include('is too long (maximum is 50 characters)')
-#       end
-#     end
-#   end
-# end
+      it 'noteable_type が空であれば登録できない' do
+        study_material_note.noteable_type = ''
+        study_material_note.valid?
+        expect(study_material_note.errors[:noteable_type]).to include("can't be blank")
+      end
+    end
+  end
+end
