ccbed2db61bc8fe27217ad630e4c34e55e4324b6
akirasuzuki899 <akira9238pg@gmail.com>
2022-01-23 23:41:24 +0900

    request(User) user factory の email のシーケンス化, new_user factory の削除, に伴う修正

--- back/spec/requests/users_spec.rb
+++ back/spec/requests/users_spec.rb
@@ -18,16 +18,17 @@ RSpec.describe "Users", type: :request do
     end
 
     context 'emailが既に登録されている場合' do
-      before { FactoryBot.create(:user) }
+      
+      before { FactoryBot.create(:user, email: 'test@example.com') }
       
       it '422 unprocessable_entity を返す' do
-        post '/api/v1/auth', params: { email: 'exist_email@test.com', password: 'password', name: 'test' }, headers: { 'ACCEPT' => 'application/json' }
+        post '/api/v1/auth', params: { email: 'test@example.com', password: 'password', name: 'test' }, headers: { 'ACCEPT' => 'application/json' }
         expect(response).to have_http_status(422)
       end
 
       it 'ユーザを登録しない' do
         expect do
-          post '/api/v1/auth', params: { email: 'exist_email@test.com', password: 'password', name: 'test' }, headers: { 'ACCEPT' => 'application/json' }
+          post '/api/v1/auth', params: { email: 'test@example.com', password: 'password', name: 'test' }, headers: { 'ACCEPT' => 'application/json' }
         end.to change(User, :count).by(0)
       end
     end
@@ -36,25 +37,25 @@ RSpec.describe "Users", type: :request do
 
   describe "login" do
 
-    before { FactoryBot.create(:user) }
+    before { FactoryBot.create(:user, email: 'test@example.com') }
 
     context '全てのパラメータが揃っている場合' do
       it '200 OK を返す' do
-        post '/api/v1/auth/sign_in', params: { email: 'exist_email@test.com', password: 'password' }, headers: { 'ACCEPT' => 'application/json' }
+        post '/api/v1/auth/sign_in', params: { email: 'test@example.com', password: 'password' }, headers: { 'ACCEPT' => 'application/json' }
         expect(response).to have_http_status(200)
       end
     end
 
     context 'password が異なる場合' do
       it '401 unauthorized を返す' do
-        post '/api/v1/auth/sign_in', params: { email: 'exist_email@test.com', password: 'tttttt' }, headers: { 'ACCEPT' => 'application/json' }
+        post '/api/v1/auth/sign_in', params: { email: 'test@example.com', password: 'tttttt' }, headers: { 'ACCEPT' => 'application/json' }
         expect(response).to have_http_status(401)
       end
     end
 
     context '登録されていない email の場合' do
       it '401 unauthorized を返す' do
-        post '/api/v1/auth/sign_in', params: { email: 'un_exist_email@test.com', password: 'password' }, headers: { 'ACCEPT' => 'application/json' }
+        post '/api/v1/auth/sign_in', params: { email: 'un_registered@example.com', password: 'password' }, headers: { 'ACCEPT' => 'application/json' }
         expect(response).to have_http_status(401)
       end
     end
