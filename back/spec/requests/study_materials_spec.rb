e05fe8df1a02b140e773d1164e724d01e726010a
akirasuzuki899 <akira9238pg@gmail.com>
2022-01-23 23:50:18 +0900

    request(StudyMaterial) user factory の email のシーケンス化, new_user factory の削除, に伴う修正

--- back/spec/requests/study_materials_spec.rb
+++ back/spec/requests/study_materials_spec.rb
@@ -3,14 +3,12 @@ require 'rails_helper'
 RSpec.describe "StudyMaterials", type: :request do
 
   describe "get" do
-    
     context "ログイン済みの場合" do
-      before do
-        FactoryBot.create(:user)
-      end
+
+      before { FactoryBot.create(:user, email: 'test@example.com') }
 
       it "200 OK を返す" do
-        user = { email: "exist_email@test.com", password: "password" }
+        user = { email: "test@example.com", password: "password" }
         auth_tokens = sign_in(user)
         get "/api/v1/study_materials", headers: auth_tokens
         expect(response).to have_http_status(200)
@@ -28,19 +26,18 @@ RSpec.describe "StudyMaterials", type: :request do
 
   describe "create" do
     context '全てのパラメータが揃っている場合' do
-      before do
-        user = FactoryBot.create(:user)
-      end
+
+      before { user = FactoryBot.create(:user, email: 'test@example.com') }
       
       it '200 OK を返す' do
-        user = { email: "exist_email@test.com", password: "password" }
+        user = { email: "test@example.com", password: "password" }
         auth_tokens = sign_in(user)
         post '/api/v1/study_materials', params: { title: 'test', rakuten_image_url: "#{Rails.root}/spec/fixtures/files/toeic1.jpeg" }, headers: auth_tokens
         expect(response).to have_http_status(200)
       end
       
       it '教材を登録する' do
-        user = { email: "exist_email@test.com", password: "password" }
+        user = { email: "test@example.com", password: "password" }
         auth_tokens = sign_in(user)
         expect do
           post '/api/v1/study_materials', params: { title: 'test', rakuten_image_url: "#{Rails.root}/spec/fixtures/files/toeic1.jpeg" }, headers: auth_tokens
