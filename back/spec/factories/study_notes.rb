387aed642d53402a2d016250af07ab41286931e1
akirasuzuki899 <akira9238pg@gmail.com>
2022-01-23 23:59:48 +0900

    model,factory(StudyNote) リレーション定義を修正、マンダラチャートと関連するノートのテストを追加

--- back/spec/factories/study_notes.rb
+++ back/spec/factories/study_notes.rb
@@ -1,16 +1,16 @@
 FactoryBot.define do
   factory :study_material_note, class: 'StudyNote' do
-    association :user, factory: :user
     association :noteable, factory: :study_material
+    user { noteable.user }
     noteable_type { 'StudyMaterial' } 
     title { 'title' }
-    rich_text { 'content has ritch text' }
+    rich_text { '{\"time\":1637831736143,\"blocks\":[{\"id\":\"sHBSl43QZU\",\"type\":\"paragraph\",\"data\":{\"text\":\"test\"}}],\"version\":\"2.22.2\"}' }
   end
   factory :mandala_item_note, class: 'StudyNote' do
-    association :user, factory: :user
     association :noteable, factory: :mandala_item 
+    user { noteable.mandala_group.mandala_chart.user }
     noteable_type { 'MandalaItem' } 
     title { 'title' }
-    rich_text { 'content has ritch text' }
+    rich_text { '{\"time\":1637831736143,\"blocks\":[{\"id\":\"sHBSl43QZU\",\"type\":\"paragraph\",\"data\":{\"text\":\"test\"}}],\"version\":\"2.22.2\"}' }
   end
 end
