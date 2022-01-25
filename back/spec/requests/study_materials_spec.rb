043d4926f010ec478fbef29ff78098d94e7d5674
akirasuzuki899 <akira9238pg@gmail.com>
2022-01-25 23:45:41 +0900

    model,request(StudyMaterial) リファクタリング

--- back/spec/requests/study_materials_spec.rb
+++ back/spec/requests/study_materials_spec.rb
@@ -1,17 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "StudyMaterials", type: :request do
+  include ActionDispatch::TestProcess::FixtureFile
+  
+  let!(:user) { FactoryBot.create(:user) }
+  let(:study_material) { FactoryBot.create(:study_material, user: user, title: "test") }
+  let(:auth_tokens) { sign_in(email: user.email, password: user.password) }
+  before { study_material.attach_rakuten_image("#{Rails.root}/spec/fixtures/files/toeic1.jpeg", study_material.title) }
 
   describe "get" do
     context "ログイン済みの場合" do
+      before { FactoryBot.create(:study_material) }
 
-      before { FactoryBot.create(:user, email: 'test@example.com') }
-
-      it "200 OK を返す" do
-        user = { email: "test@example.com", password: "password" }
-        auth_tokens = sign_in(user)
+      it "ユーザーが所有する教材を返す" do
         get "/api/v1/study_materials", headers: auth_tokens
+        json = JSON.parse(response.body).deep_symbolize_keys
+        
         expect(response).to have_http_status(200)
+        expect(json[:study_materials].count).to eq(1)
+        expect(json[:study_materials][0][:image_url]).not_to  be_nil
+        expect(json[:study_materials][0][:title]).to  eq(study_material.title)
       end
     end
 
@@ -25,23 +33,31 @@ RSpec.describe "StudyMaterials", type: :request do
   end
 
   describe "create" do
-    context '全てのパラメータが揃っている場合' do
-
-      before { user = FactoryBot.create(:user, email: 'test@example.com') }
-      
-      it '200 OK を返す' do
-        user = { email: "test@example.com", password: "password" }
-        auth_tokens = sign_in(user)
-        post '/api/v1/study_materials', params: { title: 'test', rakuten_image_url: "#{Rails.root}/spec/fixtures/files/toeic1.jpeg" }, headers: auth_tokens
-        expect(response).to have_http_status(200)
+    context "ログイン済みの場合" do
+      context '全ての属性値が揃っている場合' do
+        it '教材を作成できる' do
+          expect do
+            post '/api/v1/study_materials', 
+            params: { title: 'test2', rakuten_image_url: "#{Rails.root}/spec/fixtures/files/toeic1.jpeg" }, 
+            headers: auth_tokens
+          end.to change(user.study_materials, :count).by(1)
+          json = JSON.parse(response.body).deep_symbolize_keys
+          expect(response).to have_http_status(200)
+          expect(json[:study_material][:image_url]).not_to  be_nil
+        end
       end
-      
-      it '教材を登録する' do
-        user = { email: "test@example.com", password: "password" }
-        auth_tokens = sign_in(user)
-        expect do
-          post '/api/v1/study_materials', params: { title: 'test', rakuten_image_url: "#{Rails.root}/spec/fixtures/files/toeic1.jpeg" }, headers: auth_tokens
-        end.to change(StudyMaterial, :count).by(1)
+
+      context '属性値が無効な時' do
+        it '教材を作成しない' do
+          expect do
+            post '/api/v1/study_materials', 
+            params: { title: '', rakuten_image_url: "#{Rails.root}/spec/fixtures/files/toeic1.jpeg" }, 
+            headers: auth_tokens
+          end.to change(user.study_materials, :count).by(0)
+          json = JSON.parse(response.body).deep_symbolize_keys
+          expect(response).to have_http_status(400)
+          expect(json[:message][:title]).to  include("can't be blank")
+        end
       end
     end
   end
