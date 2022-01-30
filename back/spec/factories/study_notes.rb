c63e605c80ab5118d66bdcb1bd1ff4609e3c543e
akirasuzuki899 <akira9238pg@gmail.com>
2022-01-30 22:48:00 +0900

    reqest(StudyNote) CRUDのテスト追加

--- back/spec/factories/study_notes.rb
+++ back/spec/factories/study_notes.rb
@@ -4,13 +4,13 @@ FactoryBot.define do
     user { noteable.user }
     noteable_type { 'StudyMaterial' } 
     title { 'title' }
-    rich_text { '{\"time\":1637831736143,\"blocks\":[{\"id\":\"sHBSl43QZU\",\"type\":\"paragraph\",\"data\":{\"text\":\"test\"}}],\"version\":\"2.22.2\"}' }
+    rich_text { "{\"time\":1637831736143,\"blocks\":[{\"id\":\"sHBSl43QZU\",\"type\":\"paragraph\",\"data\":{\"text\":\"test\"}}],\"version\":\"2.22.2\"}" }
   end
   factory :mandala_item_note, class: 'StudyNote' do
     association :noteable, factory: :mandala_item 
     user { noteable.mandala_group.mandala_chart.user }
     noteable_type { 'MandalaItem' } 
     title { 'title' }
-    rich_text { '{\"time\":1637831736143,\"blocks\":[{\"id\":\"sHBSl43QZU\",\"type\":\"paragraph\",\"data\":{\"text\":\"test\"}}],\"version\":\"2.22.2\"}' }
+    rich_text { "{\"time\":1637831736143,\"blocks\":[{\"id\":\"sHBSl43QZU\",\"type\":\"paragraph\",\"data\":{\"text\":\"test\"}}],\"version\":\"2.22.2\"}" }
   end
 end
